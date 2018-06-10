fb4643de1c446dc671321beb98789a33cedf3ae3
tuong le <tuongle2604@gmail.com>
2018-06-10 22:59:15 +0700

    fix bugs no such file or directory

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var multer = require('multer');
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/images/')
+    cb(null, '/public/images')
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + '_' + Date.now() + '_' + file.originalname)
